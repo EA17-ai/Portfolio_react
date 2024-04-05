ddb9edd5b822f3d5dbff54c186e9cb9c8846320b
Abhinav_Eluri <abhinav.eluri@tuhh.de>
2024-04-05 15:29:54 +0200

    added images and tech info

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,35 @@
 import { Link } from 'react-router-dom';
-import projects from "../database.js"
+import projects from "../database.js";
 import { useAuth0 } from '@auth0/auth0-react';
+
 const Projects = () => {
-  const {user,isAuthenticated}=useAuth0()
+  const { user, isAuthenticated } = useAuth0();
 
   return (
     <>
- {/*  {isAuthenticated ?
-  (  */}       <div className='grid grid-cols-3 gap-4  bg-[linear-gradient(135deg,_#f5f7fa_0%,_#c3cfe2_100%)]'>
+      <div className='grid grid-cols-3 gap-4 bg-[linear-gradient(135deg,_#f5f7fa_0%,_#c3cfe2_100%)]'>
         {projects.map((project, index) => (
           <div key={index} className='flex flex-col p-2 m-5 bg-white rounded-lg border-2 border-black space-y-4'>
-            <img className='hover:scale-75 hover:rounded-lg w-full h-40 rounded-lg object-cover' src={`data:image/jpeg;base64,${project.image}`} alt={project.title} />
+            <img className='hover:scale-75 hover:rounded-lg w-full h-40 rounded-lg object-cover' src={project.image} alt={project.title} />
+            
             <hr />
             <p className='h-28 text-center text-xl font-bold font-serif'>{project.title}</p>
+            <div className='grid grid-cols-3 gap-4 '>
+              {project.tech.map((tech, index) => (
+                <button key={index} className='py-1 rounded-xl bg-blue-500 text-white text-xs'>{tech}</button>
+              ))}
+            </div>
             <Link className='bg-blue-700 py-2 text-white text-center rounded-lg' to={project.link}>GitHub Link</Link>
           </div>
         ))}
       </div>
-{/* ):<p className='flex justify-center items-center animate-pulse   text-3xl font-bold font-serif delay-700 h-screen bg-[linear-gradient(to_top,_#9890e3_0%,_#b1f4cf_100%)]'> Please Login to view the projects</p>
-  }
- */}      
-    </>
-  
-    
+      {/* If not authenticated, show login prompt */}
+      {/* {!isAuthenticated && (
+        <p className='flex justify-center items-center animate-pulse text-3xl font-bold font-serif delay-700 h-screen bg-[linear-gradient(to_top,_#9890e3_0%,_#b1f4cf_100%)]'>
+          Please Login to view the projects
+        </p>
+      )}
+ */}    </>
   );
 };
 
